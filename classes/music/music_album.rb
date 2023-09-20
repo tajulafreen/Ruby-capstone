ce9277072133f42511c7960d59a34a484f38b4f5
afreen shaik <affushaik9493@gmail.com>
2023-09-21 02:10:46 +0530

    update change

--- classes/music/music_album.rb
+++ classes/music/music_album.rb
@@ -1,10 +1,12 @@
-require 'date'
 require_relative '../item'
+require 'date' # Ensure the Date module is required
 
 class MusicAlbum < Item
   attr_reader :id, :on_spotify
 
   def initialize(publish_date, on_spotify)
+    # Convert the publish_date to a Date object
+    publish_date = Date.parse(publish_date)
     super(publish_date)
     @id = Random.rand(1...1000)
     @on_spotify = on_spotify
