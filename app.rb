41bab8cd9bedf9f5dae56386e7f489d383bf035d
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-20 18:14:40 +0300

    Implement add_book() method

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require_relative 'Modules/music_module'
 require_relative 'classes/music/music_album'
 require_relative 'Modules/genre_module'
+require_relative 'Modules/book_module'
+require_relative 'classes/book/book'
 
 class App
-  include MusicModule
+   include MusicModule
    include GenreModule
+   include BookModule
   def initialize
     @songs = []
     @books = []
@@ -16,6 +19,17 @@ class App
 
   def create_musicalbum
     MusicModule.add_music_album(self)
+  end
+
+  
+  def add_book(app)
+    puts 'Please enter the Publisher:'
+    publisher = gets.chomp
+    puts 'Please enter the covers state:'
+    cover_state = gets.chomp
+    puts 'Please enter the published date of the book:'
+    publish_date = gets.chomp
+    app.create_book(publisher, cover_state, publish_date)
   end
 end
 App.new
