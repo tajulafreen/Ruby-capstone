bae5be8b683c6b4e2d08c9f81ce6b2d2ddf0a918
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-21 16:00:42 +0300

    Define schema for book and label tables

--- schema.sql
+++ schema.sql
@@ -18,3 +18,26 @@ CREATE TABLE Genre (
 );
 CREATE INDEX index_music_album_item_id ON MusicAlbum(item_id);
 CREATE INDEX index_genre_item_id ON Genre(item_id);
+
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  item_id INTEGER,
+  publisher VARCHAR(255),
+  publish_date DATE,
+  cover_state VARCHAR(255),
+  archived BOOLEAN,
+  FOREIGN KEY (item_id) REFERENCES items(id)
+);
+
+CREATE INDEX idx_books_item_id ON books(item_id);
+
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  item_id INTEGER,
+  title VARCHAR(255),
+  color VARCHAR(100),
+  FOREIGN KEY (item_id) REFERENCES items(id)
+);
+
+CREATE INDEX idx_labels_item_id ON labels(item_id);
